70e47f1e90b8ee49614b52a1db2b70237d1abc5f
Cory Baumer <cory.baumer@vuria.com>
2021-03-06 16:34:53 -0700

    negative() is unnecessary, forward now clearer than positive()

--- Models/Piece/Pawn.php
+++ Models/Piece/Pawn.php
@@ -8,16 +8,16 @@ class Pawn extends Piece
     public function getAvailableMoves(): array
     {
         $moves = [
-            new Move($this->rank + $this->positive(1), $this->file),
+            new Move($this->rank + $this->forward(1), $this->file),
         ];
 
         $pawnRank = $this->color === Colors::black ? Ranks::seven : Ranks::two;
         if ($this->rank === $pawnRank) {
-            $moves[] = new Move($this->rank + $this->positive(2), $this->file); // Two step advance
+            $moves[] = new Move($this->rank + $this->forward(2), $this->file); // Two step advance
         }
 
-        $this->addCaptureMove($moves, $this->positive(1));
-        $this->addCaptureMove($moves, $this->negative(1));
+        $this->addCaptureMove($moves, 1);
+        $this->addCaptureMove($moves, -1);
 
         return $moves;
     }
@@ -27,19 +27,14 @@ class Pawn extends Piece
      */
     private function addCaptureMove(array &$moves, int $fileOffset)
     {
-        $opponent = $this->board->getPiece($this->rank + $this->positive(1), $this->file + $fileOffset);
+        $opponent = $this->board->getPiece($this->rank + $this->forward(1), $this->file + $fileOffset);
         if ($opponent) {
             $moves[] = new Move($opponent->getRank(), $opponent->getFile());
         }
     }
 
-    private function positive(int $i): int
+    private function forward(int $i): int
     {
         return $this->color === Colors::black ? $i * -1 : $i;
-    }
-
-    private function negative(int $i): int
-    {
-        return $this->color === Colors::black ? $i : $i * -1;
     }
 }
