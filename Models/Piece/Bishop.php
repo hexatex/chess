f8a39af5826cb2af47513862d0b25c5c2fca54b6
Cory Baumer <cory.baumer@vuria.com>
2021-03-05 20:47:31 -0700

    remove todo

--- Models/Piece/Bishop.php
+++ Models/Piece/Bishop.php
@@ -9,11 +9,6 @@ class Bishop extends Piece
     {
         $allOffBoard = false;
 
-        /*
-         * Todo the ranks and files are shared between black & white, +/- needs to be inverted conditionally,
-         * same goes for >/< for the Pawn, and maybe other places
-         */
-
         $moves = [];
         for ($i = 1; $i >= Ranks::eight + 1 || $allOffBoard; $i++) {
             $allOffBoard = !$this->addMove($moves, new Move($this->rank + $i, $this->file + $i))
